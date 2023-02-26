c733d760575a0cdf3dc64ea218a44bb7d21bfd4b
Zivzhang <letsdancezzhang@gmail.com>
2023-02-26 16:56:16 +0800

    gyhfyytuf

---
+++ vite.config.js
@@ -0,0 +1,50 @@
+import { defineConfig } from "vite";
+import path, { resolve } from "path";
+
+export default defineConfig({
+  base: "./",
+  publicPath: process.env.NODE_ENV === "production" ? "/vite-css/" : "./",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "assets": path.resolve(__dirname, "src/assets/"),
+      "img": path.resolve(__dirname, "src/img"),
+      "styles": path.resolve(__dirname, "src/styles/"),
+      "pages": path.resolve(__dirname, "src/pages/"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        topic: resolve(__dirname, "topic.html"),
+      },
+      output: {
+        chunkFileNames: "js/[name].[hash].js",
+        entryFileNames: "js/[name].[hash].js",
+        assetFileNames: (info) => {
+          let type = info.name.split(".")[1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico|webp/i.test(type)) {
+            type = "img";
+          } else if (/ttf|eot|woff|woff2/i.test(type)) {
+            type = "fonts"
+          } else if (/mp4|webm|ogg/i.test(type)) {
+            type = "video"
+          } else if (/mp3|wav/i.test(type)) {
+            type = "sound"
+          }
+          return `${type}/[name]-[hash].[ext]`;
+        },
+      },
+    },
+    cssCodeSplit: false,
+  },
+  cssPreprocessOptions: {
+    scss: {
+      // additionalData: '@import "./styles/variable.scss";',
+    },
+  },
+  css: {
+    devSourceMap: true,
+  },
+});
